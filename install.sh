b1ca0310b36aac29cd0aef1fe1f58216d670e5d8
githubdmn <dmninbox@gmail.com>
2021-11-29 19:20:13 +0100

    update

--- install.sh
+++ install.sh
@@ -23,12 +23,12 @@ oh-my-bash() {
 }
 vlc() { sudo apt install vlc -y; }
 brave() {
-	sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-	echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-	update
-	sudo apt install brave-browser
+	sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg;
+	echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list;
+	update;
+	sudo apt install brave-browser;
 }
-dropbox() { wget -O ~/ - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf - }
+dropbox() { wget -O ~/ - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -; }
 
 upgrade
 build_essential
