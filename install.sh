57587dc81107dd0e26efa265d5ba80e3275fe194
githubdmn <dmninbox@gmail.com>
2022-03-22 07:52:11 +0100

    install nvm ohmyzsh vscode

---
+++ install.sh
@@ -0,0 +1,4 @@
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+wget https://code.visualstudio.com/sha/download?build=stable&os=darwin-universal
