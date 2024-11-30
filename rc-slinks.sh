541cdd1c2f8a034da3b6e7bb9d9fcb2988d552d6
githubdmn <dmninbox@gmail.com>
2024-11-30 19:57:21 +0100

    updated

--- rc-slinks.sh
+++ rc-slinks.sh
@@ -2,28 +2,44 @@
 
 user=dmn
 
-function create_links () {
-	sudo ln -sv ${HOME}/dotfiles/nanorc ${HOME}/.nanorc;
-	sudo ln -sv ${HOME}/dotfiles/vimrc ${HOME}/.config/nvim/init.vim;
-	sudo ln -sv ${HOME}/dotfiles/bashrc ${HOME}/.bashrc;
-	sudo ln -sv ${HOME}/dotfiles/tmux.conf ${HOME}/.tmux.conf;
-	sudo ln -sv ${HOME}/dotfiles/terminalrc ${HOME}/.config/xfce4/terminal/terminalrc;
-} 
+function create_links() {
+	echo "Creating symbolic links..."
+	# Create symlinks for configuration files
+	ln -sv ${HOME}/dotfiles/nanorc ${HOME}/.nanorc
+	ln -sv ${HOME}/dotfiles/vimrc ${HOME}/.config/nvim/init.vim
+	ln -sv ${HOME}/dotfiles/bashrc ${HOME}/.bashrc
+	ln -sv ${HOME}/dotfiles/tmux.conf ${HOME}/.tmux.conf
+	ln -sv ${HOME}/dotfiles/terminalrc ${HOME}/.config/xfce4/terminal/terminalrc
 
+	# Ensure that your custom lambda.theme.sh is used, even after git pull
+	echo "Ensuring custom theme is used..."
+	# Backup the existing theme (optional)
+	if [ -f ~/.oh-my-bash/themes/lambda/lambda.theme.sh ]; then
+		echo "Backing up existing lambda.theme.sh..."
+		mv ~/.oh-my-bash/themes/lambda/lambda.theme.sh ~/.oh-my-bash/themes/lambda/lambda.theme.sh.bak
+	fi
+
+	# Create a symlink to your dotfiles theme
+	echo "Creating symlink for lambda.theme.sh..."
+	ln -sf ~/dotfiles/lambda.theme.sh ~/.oh-my-bash/themes/lambda/lambda.theme.sh
+}
 
 function remove_files() {
-	sudo rm -rv ${HOME}/.nanorc;
-	sudo rm -rv ${HOME}/.vimrc;
-	sudo rm -rv ${HOME}/.bashrc;
-	sudo rm -rv ${HOME}/.tmux.conf;
-	sudo rm -rv ${HOME}/.config/xfce4/terminal/terminalrc;
+	echo "Removing old configuration files..."
+	rm -rv ${HOME}/.nanorc
+	rm -rv ${HOME}/.vimrc
+	rm -rv ${HOME}/.bashrc
+	rm -rv ${HOME}/.tmux.conf
+	rm -rv ${HOME}/.config/xfce4/terminal/terminalrc
 }
 
 function prepare_terminal_theme() {
-	sudo mkdir -p ${HOME}/.local/share/xfce4/terminal/colorschemes
-	sudo cp -v ${HOME}/dotfiles/catppuccin/* ${HOME}/.local/share/xfce4/terminal/colorschemes
+	echo "Setting up terminal theme..."
+	mkdir -p ${HOME}/.local/share/xfce4/terminal/colorschemes
+	cp -v ${HOME}/dotfiles/catppuccin/* ${HOME}/.local/share/xfce4/terminal/colorschemes
 }
 
-remove_files;
-prepare_terminal_theme;
-create_links;
+# Run the functions
+remove_files
+prepare_terminal_theme
+create_links
