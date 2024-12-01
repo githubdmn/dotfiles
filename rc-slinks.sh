f2f389d58bb0efab7cd5e9bac2a3da2daa7b2f89
Damian <dmninbox@gmail.com>
2024-12-01 20:10:48 +0100

    Debian (#1)
    * date format
    
    * updated
    
    * simple intallation
    
    * additional settings
    
    * update
    
    * Update vimrc
    
    * rc files updated
    
    * tab indent length changed and NERDTreeRefreshRoot
    
    * vimrc nerdtree refresh and toogle
    
    * vimrc nerdtree refresh and toogle
    
    * go home path added
    
    * set github name and email in local project
    
    * function naming fixed
    
    * added sqlite alias
    
    * added sqlite alias
    
    * upgrade system
    
    * new alias; update, upgrade and autoremove
    
    * vscode install
    
    * Official docker install
    
    * install docker sh
    
    * docker-ce-rootless-extras
    
    * docker-compose install
    
    * run docker rootless
    
    * neovim init
    
    * ray-x/go.nvim plugin added
    
    * go plug suspended
    
    * theme change
    
    * tor alias added
    
    * Automatic toggling between line number modes
    
    * home env var used
    
    * fixed path
    
    * python3 alias
    
    * install ffmpeg
    
    * terminal font size 14
    
    * theme changed
    
    * disabled tmux start
    
    * install docker debian
    
    * install docker ubuntu
    
    * disabled "Plugin 'c.vim'
    
    * added: java cli
    
    * fixed: autoremove \nadded: ffmpeg
    
    * added: autoread
    
    * FontName=Noto Mono Bold 10
    
    * vs code
    
    * xubuntu side panel
    
    * terminal font changed
    
    * font changed
    
    * system color theme
    
    * Update vscode.settings.json
    
    * updated terminal
    
    * features excluded
    
    * sqlite added to path
    
    * added: sqlite
    
    * theme changed
    
    * added: catppuccin theme
    
    * theme configured
    
    * added: nvim `vimrc`
    
    * added: java local
    
    * mod 23-09
    
    * create bash scripts
    * sqlite
    
    * Update install.sh
    
    `[install_]` rearranged
    
    * Update bashrc
    
    updated alias
    
    * Update install.sh
    
    * Update bashrc
    
    * iris added
    
    * updated install docker debian
    
    * gradle 8.10.2
    
    * sdkman updated
    
    * sdkman install java
    
    * check is sdkman is installed
    
    * fromatted
    
    * install deno fixed
    
    * `gdb` added
    
    * omb custom lambda theme
    
    * vscode
    
    * xfce-autostart
    
    * local vscode
    
    * added autostart

---
+++ rc-slinks.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+user=dmn
+
+function create_links() {
+	echo "Creating symbolic links..."
+	# Create symlinks for configuration files
+	ln -sv ${HOME}/dotfiles/nanorc ${HOME}/.nanorc
+	ln -sv ${HOME}/dotfiles/vimrc ${HOME}/.config/nvim/init.vim
+	ln -sv ${HOME}/dotfiles/bashrc ${HOME}/.bashrc
+	ln -sv ${HOME}/dotfiles/tmux.conf ${HOME}/.tmux.conf
+	ln -sv ${HOME}/dotfiles/terminalrc ${HOME}/.config/xfce4/terminal/terminalrc
+
+	# Ensure that your custom lambda.theme.sh is used, even after git pull
+	echo "Ensuring custom theme is used..."
+	# Backup the existing theme (optional)
+	if [ -f ~/.oh-my-bash/themes/lambda/lambda.theme.sh ]; then
+		echo "Backing up existing lambda.theme.sh..."
+		mv ~/.oh-my-bash/themes/lambda/lambda.theme.sh ~/.oh-my-bash/themes/lambda/lambda.theme.sh.bak
+	fi
+
+	# Create a symlink to your dotfiles theme
+	echo "Creating symlink for lambda.theme.sh..."
+	ln -sf ~/dotfiles/lambda.theme.sh ~/.oh-my-bash/themes/lambda/lambda.theme.sh
+}
+
+function remove_files() {
+	echo "Removing old configuration files..."
+	rm -rv ${HOME}/.nanorc
+	rm -rv ${HOME}/.vimrc
+	rm -rv ${HOME}/.bashrc
+	rm -rv ${HOME}/.tmux.conf
+	rm -rv ${HOME}/.config/xfce4/terminal/terminalrc
+}
+
+function prepare_terminal_theme() {
+	echo "Setting up terminal theme..."
+	mkdir -p ${HOME}/.local/share/xfce4/terminal/colorschemes
+	cp -v ${HOME}/dotfiles/catppuccin/* ${HOME}/.local/share/xfce4/terminal/colorschemes
+}
+
+#!/bin/bash
+
+function link_autostart_entries() {
+  echo "Linking autostart .desktop files..."
+
+  # Source directory containing the .desktop files
+  local source_dir="${HOME}/dotfiles/config/autostart"
+  # Destination directory for autostart files
+  local dest_dir="${HOME}/.config/autostart"
+
+  # Ensure the destination directory exists
+  mkdir -p "$dest_dir"
+
+  # Check if the source directory exists
+  if [ ! -d "$source_dir" ]; then
+    echo "Source directory '$source_dir' does not exist. Please check the path."
+    return 1
+  fi
+
+  # Iterate over all .desktop files in the source directory
+  for file in "$source_dir"/*.desktop; do
+    # Check if there are any .desktop files
+    if [ -e "$file" ]; then
+      # Create a symbolic link in the destination directory
+      ln -sfv "$file" "$dest_dir/"
+    else
+      echo "No .desktop files found in '$source_dir'."
+    fi
+  done
+
+  echo "Autostart .desktop files linked successfully."
+}
+
+remove_files
+prepare_terminal_theme
+create_links
+link_autostart_entries
